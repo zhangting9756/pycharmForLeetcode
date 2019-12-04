e87031b0e42111ff654100d422d8e7192e10c29f
zhangting9756 <tingzhangguo@163.com>
2019-12-04 15:27:09 +0800

    leetcode字符串中的第一个唯一字符

--- src/charString.py
+++ src/charString.py
@@ -1,7 +1,7 @@
 # -*- coding: UTF-8 -*-
 import numpy as np
 import re
-
+import collections
 """验证回文字符串,给定一个字符串，验证它是否是回文串，只考虑字母和数字字符，可以忽略字母的大小写。"""
 class Solution(object):
     def isPalindrome(self, s):
@@ -22,9 +22,19 @@ class Solution(object):
             i += 1
             j -= 1
         return True
-
+"""字符串中的第一个唯一字符
+给定一个字符串，找到它的第一个不重复的字符，并返回它的索引。如果不存在，则返回 -1。"""
+class Solution1:
+    def firstUniqChar(self, s: str) -> int:
+        if(len(s)==0):
+            return -1
+        dic = collections.Counter(s)
+        for i in range(0, len(s)):
+            if dic[s[i]] < 2:
+                return i
+        return -1
 
 if __name__ == '__main__':
-    a: str = "0p"
-    c = Solution()
-    print(c.isPalindrome(a))
+    a: str = "loveleetcode"
+    c = Solution1()
+    print(c.firstUniqChar(a))
