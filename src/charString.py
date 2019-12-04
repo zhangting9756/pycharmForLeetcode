61c24c23ca1bd56538a7017600b027b001ceab08
zhangting9756 <tingzhangguo@163.com>
2019-12-04 23:34:36 +0800

    测试家里pycharm

--- src/charString.py
+++ src/charString.py
@@ -25,7 +25,7 @@ class Solution(object):
 """字符串中的第一个唯一字符
 给定一个字符串，找到它的第一个不重复的字符，并返回它的索引。如果不存在，则返回 -1。"""
 class Solution1:
-    def firstUniqChar(self, s: str) -> int:
+    def firstUniqChar(self, s):
         if(len(s)==0):
             return -1
         dic = collections.Counter(s)
