a4118f06396770935109139d9603be62e7dcc5e7
zhangting9756 <tingzhangguo@163.com>
2019-12-12 10:18:09 +0800

    增加最长公共前缀

--- src/charString.py
+++ src/charString.py
@@ -76,13 +76,45 @@ class Solution3:
         operator.gt(a, b)   //a>b
         """
         return operator.eq(dic1,dic2)
+"""实现 strStr()
+给定一个 haystack 字符串和一个 needle 字符串，在 haystack 字符串中找出 needle 字符串出现的第一个位置 (从0开始)。如果不存在，则返回  -1。"""
+class Solution4:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if needle == "":
+            return 0
+        if haystack == needle:
+            return 0
+        if needle not in haystack:
+            return -1
+        else:
+            h = haystack.split(needle)
+            return len(h[0])
+"""最长公共前缀"""
+class Solution5(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        # 判断是否为空
+        if not strs:
+            return ''
+        # 在使用max和min的时候已经把字符串比较了一遍
+        # 当前列表的字符串中，每个字符串从第一个字母往后比较直至出现ASCII码 最小的字符串
+        s1 = min(strs)
+        # 当前列表的字符串中，每个字符串从第一个字母往后比较直至出现ASCII码 最大的字符串
+        s2 = max(strs)
+        # 使用枚举变量s1字符串的每个字母和下标
+        for i, c in enumerate(s1):
+            # 比较是否相同的字符串，不相同则使用下标截取字符串
+            if c != s2[i]:
+                return s1[:i]
+        return s1
 
 if __name__ == '__main__':
-    """
-    a: str = "loveleetcode"
-    c = Solution1()
-    print(c.firstUniqChar(a))
-    """
-    lists = ['a', 'a', 'b', '5', '6', '7', '5']
-    a = Solution3()
-    print(a.isAnagram("rat","car"))
+
+    a = ["dog","racecar","car"]
+    b = "ll"
+    c = Solution5()
+    print(c.longestCommonPrefix(a))
+
